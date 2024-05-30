8f78e8a9c66ea7b861d8e5c9cbe53c552d2f730c
Celeste Mariel Soto <celestemairel28@gmail.com>
2024-05-29 22:47:09 -0300

    wip

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ClinicApp.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Addres { get; set; } = "";
+        public DateTime CreatedAt { get; set; }
+    }
+}
