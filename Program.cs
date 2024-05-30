f6e02fedef2fcd9c9bd94c869ccb1d1eec3a20f3
Mauro Cabrera <maurocabrera@diloog.com.ar>
2024-05-29 22:51:39 -0300

    WIP

--- Program.cs
+++ Program.cs
@@ -1,7 +1,20 @@
+using ClinicApp.Sevices;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using ClinicApp.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    options.UseSqlServer(connectionString);
+});
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
---
+++ Sevices/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using ClinicApp.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClinicApp.Sevices
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var admin = new IdentityRole("admin");
+            admin.NormalizedName = "admin";
+
+            var client = new IdentityRole("client");
+            admin.NormalizedName = "client";
+
+            var patient = new IdentityRole("patient");
+            admin.NormalizedName = "patient";
+
+            builder.Entity<IdentityRole>().HasData(admin, client, patient);
+
+        }
+    }
+
+    
+}
